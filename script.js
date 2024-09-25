e0e1e23d380c901ef0b104b835b6eced99fc98d2
Mouli1807 <tapadarmouli@gmail.com>
2024-09-25 13:19:04 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,119 @@
+const typingText = document.querySelector('.typing-test p')
+const input = document.querySelector('.wrapper .input-field')
+const time = document.querySelector('.time span b')
+const mistakes = document.querySelector('.mistake span')
+const wpm = document.querySelector('.wpm span')
+const cpm = document.querySelector('.cpm span')
+const btn = document.querySelector('button');
+
+//set value
+let timer;
+let maxTime= 60;
+let timeLeft = maxTime;
+let charIndex = 0;
+let mistake =0;
+let isTyping = false;
+
+
+function loadParagraph(){
+    const paragraph= [ "Avoid daydreaming about the years to come.",
+        "You are the most important person in your whole life.",
+        "The quick brown fox jumps over the lazy dog.",
+        "Always be true to who you are, and ignore what other people have to say about you.",
+        "He walked through the forest quietly, listening to the sounds of nature.",
+        "Only demonstrate your strength when its really required.",
+        "How many times have you tried to type faster, only to make more mistakes? Practice makes perfect!",
+        "Typing fast requires practice, patience, and a lot of focus."
+];
+
+const randomIndex = Math.floor(Math.random()*paragraph.length);
+typingText.innerHTML='';
+for(const char of paragraph[randomIndex]){
+console.log(char);
+typingText.innerHTML+= `<span>${char}</span>`;
+}
+typingText.querySelectorAll('span')[0].classList.add('active');
+document.addEventListener('keydown',()=>input.focus());
+typingText.addEventListener("click",()=>{
+    input.focus()})
+}
+
+//Handle user input
+function initTyping(){
+    const char= typingText.querySelectorAll('span');
+    const typedChar = input.value.charAt(charIndex);
+    if(charIndex < char.length && timeLeft >0){
+
+        if(!isTyping){
+            timer = setInterval(initTime,1000);
+            isTyping=true;
+        }
+
+        if(char[charIndex].innerText === typedChar){
+            char[charIndex].classList.add('correct');
+            console.log("correct");
+        }
+        else{
+            mistake++ ;
+            char[charIndex].classList.add('incorrect');
+            console.log("incorrect");
+        }
+        charIndex++;
+        char[charIndex].classList.add('active');
+
+        mistakes.innerText = mistake;
+        cpm.innerText = charIndex- mistake;
+    }
+    else{
+clearInterval(timer);
+input.value='';
+    }
+}
+
+function initTime(){
+    if(timeLeft>0){
+        timeLeft--;
+        time.innerText=timeLeft;
+        let wpmVal = Math.round(((charIndex - mistake)/5) /(maxTime - timeLeft)*60);
+        wpm.innerText = wpmVal;
+    }
+    else{
+        clearInterval(timer);
+    }
+}
+
+function reset(){
+    loadParagraph();
+    clearInterval(timer);
+    timeLeft = maxTime;
+    time.innerText= timeLeft;
+    input.value='';
+    charIndex = 0;
+    mistake =0;
+    isTyping = false;
+    wpm.innerText=0;
+    cpm.innerText=0;
+    mistakes.innerText=0;
+}
+
+input.addEventListener('keydown', function (event) {
+    if (event.key === "Backspace") {
+        event.preventDefault(); // Prevent default backspace behavior
+        console.log("Backspace pressed, but default action is prevented.");
+    }
+});
+
+input.addEventListener("input",initTyping);
+btn.addEventListener("click",reset);
+loadParagraph();
+
+function openSidebar() {
+    document.getElementById("sidebar").style.width = "250px";
+    document.getElementById("main-content").style.marginRight = "250px"; // Shift the content to the left
+  }
+  
+  function closeSidebar() {
+    document.getElementById("sidebar").style.width = "0";
+    document.getElementById("main-content").style.marginRight = "0"; // Reset the margin when closing
+  }
+  
